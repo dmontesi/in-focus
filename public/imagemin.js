f7bbd14fd838e426e41f97f362014b6fd7b93a89
Dan M <danmontesinos6@gmail.com>
2022-07-14 11:17:49 -0400

    update packages / imagemin.js

--- public/imagemin.js
+++ public/imagemin.js
@@ -1,37 +1,25 @@
-const imagemin = require('imagemin'), // The imagemin module.
-	webp = require('imagemin-webp'), // imagemin's WebP plugin.
-	outputFolder = './img/WebP', // Output folder
-	PNGImages = './img/*.png', // PNG images
-	JPEGImages = './img/*.jpg'; // JPEG images
+import imagemin from 'imagemin'
+import imageminWebp from 'imagemin-webp'
+import imageminJpegtran from 'imagemin-jpegtran'
+import imageminPngquant from 'imagemin-pngquant'
 
-// WEBP
-// imagemin([PNGImages], outputFolder, {
-// 	plugins: [
-// 		webp({
-// 			lossless: true, // Losslessly encode images
-// 		}),
-// 	],
-// });
+// WebP
+(async () => {
+	await imagemin(['./img/*.{jpg,png}'], {
+		destination: './img/build/',
+		plugins: [imageminWebp({ quality: 50 }),],
+	})
 
-// imagemin([JPEGImages], outputFolder, {
-// 	plugins: [
-// 		webp({
-// 			quality: 65, // Quality setting from 0 to 100
-// 		}),
-// 	],
-// });
+	console.log('Images optimized')
+})()
 
-// JPGs
-const imageminMozjpeg = require('imagemin-mozjpeg');
-const output = './img/small';
-
-const optimiseJPEGImages = () =>
-	imagemin([JPEGImages], output, {
-		plugins: [
-			imageminMozjpeg({
-				quality: 70,
-			}),
-		],
-	});
-
-optimiseJPEGImages().catch((error) => console.log(error));
+// Jpegs & Pngs
+const files = await imagemin(['./img/*.{jpg,png}'], {
+	destination: './img/build/',
+	plugins: [
+		imageminJpegtran(),
+		imageminPngquant({
+			quality: [0.6, 0.8]
+		}),
+	]
+});
